4f08cd983e7f59e3a03714801ab46c43e2eddec3
XamuelUG <samweko@gmail.com>
2021-08-26 00:54:17 +0300

    heroku error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const conn = require("./database/db");
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
