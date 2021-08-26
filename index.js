f903ee42fe1d4de99a7c56938d5a4c243a8c12c4
MulindwaDenis13 <mulindwadenis13@gmail.com>
2021-08-26 08:23:21 +0300

    rx

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.use("/api/user/admin/", require("./api/admin"));
 app.use("/api/user/all/", require("./api/users"));
 app.use("/api/user/sale/", require("./api/sale"));
 
-app.get("/", (req, res) => {
-  res.send("Xamuel_UG");
-});
-
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
