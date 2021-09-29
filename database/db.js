5117e65a38beaa51b6974b8cac0cf2f4fcef04da
XamuelUG <samweko@gmail.com>
2021-09-29 21:35:11 +0300

    change db

--- database/db.js
+++ database/db.js
@@ -2,15 +2,23 @@ const path = require("path");
 const fs = require("fs");
 const mysql = require("mysql8");
 
+// const MYSQL_CONFIG = {
+//   host: "38.17.53.113",
+//   user: "admin",
+//   password: "YGJTOBYf",
+//   database: "fhp",
+//   port: 16662,
+//   ssl: {
+//     ca: fs.readFileSync(path.join(__dirname, "ca.pem")),
+//   },
+// };
 const MYSQL_CONFIG = {
-  host: "38.17.53.113",
-  user: "admin",
-  password: "YGJTOBYf",
-  database: "fhp",
-  port: 16662,
-  ssl: {
-    ca: fs.readFileSync(path.join(__dirname, "ca.pem")),
-  },
+  host: "104.198.133.80",
+  user: "dreamscom",
+  password: "dreamscom",
+  // ssl: {
+  //   ca: fs.readFileSync(path.join(__dirname, "client-key.pem")),
+  // },
 };
 
 const conn = mysql.createConnection(MYSQL_CONFIG);
